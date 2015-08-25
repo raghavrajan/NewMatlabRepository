6aa6939979ce2d65c212ff66d3bc04cb1ba16849
Raghav <raghav@iiserpune.ac.in>
2015-08-25 21:56:06 +0530

    Made a change to the Fix syllable boundaries code that is part of AutoSongSegmentLabel. I made it calculate LogAmplitude only in the 1000-4000Hz range (default, but can be changed in AutoSongSegmentLabel GUI) for adjusting syllable boundaries.

--- songanalysis_PlumpieCopy/AutoSongSegmentLabel/ASSLGetIndividualSyllData.m
+++ songanalysis_PlumpieCopy/AutoSongSegmentLabel/ASSLGetIndividualSyllData.m
@@ -12,12 +12,12 @@ for i = 1:length(handles.ASB.UniqueSyllLabels),
             if (handles.ASB.AllSyllIndices(Matches(j),1) ~= handles.ASB.AllSyllIndices(Matches(j-1),1))
                 [RawData, Fs] = ASSLGetRawData(handles.ASB.DirName, handles.ASB.FileName{handles.ASB.AllSyllIndices(Matches(j),1)}, handles.ASB.FileType, handles.ASB.SongChanNo);
                 Time = (1:1:length(RawData))/Fs * 1000;
-                [LogAmplitude] = ASSLCalculateLogAmplitude(RawData, Fs, Time, handles.ASB.FFTWinSizeSegmenting, handles.ASB.FFTWinOverlapSegmenting);
+                [LogAmplitude] = ASSLCalculateLogAmplitudeAronovFee(RawData, Fs, Time, handles.ASB.FFTWinSizeSegmenting, handles.ASB.FFTWinOverlapSegmenting);
             end
         else
             [RawData, Fs] = ASSLGetRawData(handles.ASB.DirName, handles.ASB.FileName{handles.ASB.AllSyllIndices(Matches(j),1)}, handles.ASB.FileType, handles.ASB.SongChanNo);
             Time = (1:1:length(RawData))/Fs * 1000;
-            [LogAmplitude] = ASSLCalculateLogAmplitude(RawData, Fs, Time, handles.ASB.FFTWinSizeSegmenting, handles.ASB.FFTWinOverlapSegmenting);
+            [LogAmplitude] = ASSLCalculateLogAmplitudeAronovFee(RawData, Fs, Time, handles.ASB.FFTWinSizeSegmenting, handles.ASB.FFTWinOverlapSegmenting);
         end
         
         StartIndex = find(Time >= (handles.ASB.SyllOnsets{handles.ASB.AllSyllIndices(Matches(j),1)}(handles.ASB.AllSyllIndices(Matches(j),2)) - PreTime), 1, 'first');
